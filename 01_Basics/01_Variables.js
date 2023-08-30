7742479c90cea8f96ed3b89abaf9aae8efca37b8
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-08-30 11:01:02 +0000

    Variables

---
+++ 01_Basics/01_Variables.js
@@ -0,0 +1,6 @@
+let accountId=1234
+let accountEmail="ansari34@gmail.com"
+let accountPass="4321"
+accountCity="Mumbai"
+let accountState
+console.table([accountId,accountEmail,accountPass,accountCity,accountState])
