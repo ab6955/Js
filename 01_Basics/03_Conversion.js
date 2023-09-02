d1dee709fb09677b9843ed147ded585f590e1fbf
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-02 13:06:40 +0000

    Type_conversion

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -1,3 +1,26 @@
-let score=33
+let score = 33
+let age = "abdullah"
+let valueInNumber = Number(age)
 
-console.log(typeof(score))
+// console.log(typeof(score))
+// console.log(typeof age)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
+
+
+let isLoogedIn = false
+let booleanIsLoggedIn = Boolean(isLoogedIn)
+
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+let number=23
+let stringNumber=String( number)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+let string=""
+let numberInString=Number(string)
+console.log(typeof numberInString);
+console.log(numberInString);
