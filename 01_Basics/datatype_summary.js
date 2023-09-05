ccc12ef32c1efff40b30bf132db2ec23234946c6
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-05 13:09:17 +0000

    data type summary

---
+++ 01_Basics/datatype_summary.js
@@ -0,0 +1,79 @@
+//  In js datatypes can be classified into 2 types
+
+// 1) Primitive
+// 2) Non-primitive (Reference)
+
+
+
+
+// #########  Primitive Data types   #####
+
+// 1) String 
+const userName = "Hitesh"
+
+//  2) Number
+const score = 24
+const scoreValue = 34.32
+
+//  3) Boolean
+const isLoggedIn = true
+
+//  4) null
+const temperature = null
+
+// 5) undefined
+let userEmail;
+
+// 6) symbol
+const id = Symbol('45677')
+const userId = Symbol('45677')
+
+// console.log(id != userId);
+
+// 7) BigInt
+const bigNumber = 93856239572357
+
+
+
+
+// #########  Non-Primitive Data types   #####
+
+// 1) Array[]
+const heros = ["Captain America", "Iron Man", "Thor", "Hulk"]
+
+// 2) Object{}
+{
+    Name: "Abdullah"
+    age: 21
+}
+
+// 3) Function
+
+const myfunction = function () {
+    console.log("Kaise ho");
+}
+
+
+// console.log(typeof userName);
+// console.log(typeof score);
+// console.log(typeof scoreValue);
+// console.log(typeof isLoggedIn);
+// console.log(typeof temperature);
+// console.log(typeof userEmail);
+// console.log(typeof userId);
+// console.log(typeof bigNumber);
+
+console.log(typeof heros);
+console.log(typeof Object);
+console.log(typeof myfunction);
+
+
+
+
+
+
+
+//  null => object
+//  Array => function
+//  function => function object
+//  object => function
