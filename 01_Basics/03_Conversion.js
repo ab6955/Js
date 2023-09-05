b1d3b4d2bb5e829f05b3a7e3e035cbe366b3014c
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-05 09:52:51 +0000

    Done

--- 01_Basics/03_Conversion.js
+++ 01_Basics/03_Conversion.js
@@ -1,26 +1,35 @@
-let score = 33
-let age = "abdullah"
-let valueInNumber = Number(age)
+// let score = 33
+// let age = "abdullah"
+// let valueInNumber = Number(age)
 
-console.log(typeof(score))
-console.log(typeof age)
-console.log(typeof(valueInNumber))
-console.log(valueInNumber);
-
-
-let isLoogedIn = false
-let booleanIsLoggedIn = Boolean(isLoogedIn)
-
-console.log(typeof(booleanIsLoggedIn));
-console.log(booleanIsLoggedIn);
-
-let number=23
-let stringNumber=String( number)
-console.log(typeof stringNumber);
-console.log(stringNumber);
+// console.log(typeof(score))
+// console.log(typeof age)
+// console.log(typeof(valueInNumber))
+// console.log(valueInNumber);
 
 
-let string=""
-let numberInString=Number(string)
-console.log(typeof numberInString);
-console.log(numberInString);
+// let isLoogedIn = false
+// let booleanIsLoggedIn = Boolean(isLoogedIn)
+
+// console.log(typeof(booleanIsLoggedIn));
+// console.log(booleanIsLoggedIn);
+
+// let number=23
+// let stringNumber=String( number)
+// console.log(typeof stringNumber);
+// console.log(stringNumber);
+
+
+// let string=undefined
+// let numberInString=Number(string)
+// console.log(typeof numberInString);
+// console.log(numberInString);
+
+
+/*           Operation           */
+console.log(2**3);
+console.log(5%2);
+
+let str1="hello"
+let str2=" Abdullah"
+console.log(str1+str2);
