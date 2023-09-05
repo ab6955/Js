8633e17825210819b2b81bc005dd52508ae42ad2
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-05 11:38:39 +0000

    Conversion

---
+++ 01_Basics/04_conversion.js
@@ -0,0 +1,19 @@
+// console.log(8>2);
+
+console.log("4">3);
+console.log(9>"2");
+
+
+/* Try to avoid these types of conversion */
+
+console.log(null>0);
+console.log(null<0);
+console.log(null==0);
+console.log(null>=0);
+console.log(null<=0);
+
+console.log(undefined > 0);
+console.log(undefined < 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined <= 0);
