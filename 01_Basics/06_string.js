802f0aa1678c8fefef63e946e44021af6c015612
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-06 13:11:34 +0000

    strings

---
+++ 01_Basics/06_string.js
@@ -0,0 +1,53 @@
+const name="Abdullah"
+const repoCount=43
+
+// console.log(name+repo);
+
+// console.log(`This is ${name} ansari and ${repoCount}`);
+
+const gameName=new String("Abdullahcr Ansari")
+
+// console.log(gameName[9]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.toLowerCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf("r"));
+
+// const string1=gameName.substring(0,5)
+// console.log(string1);
+
+// const string2=gameName.slice(4)
+// const string2=gameName.slice(4,9)
+// const string2=gameName.slice(-4)
+// const string2=gameName.slice(-6,-1)
+// console.log(string2);
+
+//  trim method
+// const newStringOne="    Abdullah    "
+// console.log(newStringOne);
+// console.log(newStringOne.trimStart());
+// console.log(newStringOne.trimEnd());
+// console.log(newStringOne.trim());
+
+// index method
+// const sentence="My name is Abdullah ansari"
+// index=4
+// console.log(`The value of ${index} returns as ${sentence.at(index)}`);    
+// index=-4
+// console.log(`The value of ${index} returns as ${sentence.at(index)}`);    
+
+// charAt()method  
+// charAT() methos is used  for characters only while at() used for array also..
+// const sentence="My name is Abdullah ansari"
+// index=4
+// console.log(`The value of ${index} returns as ${sentence.at(index)}`);    
+// index=-4
+// console.log(`The value of ${index} returns as ${sentence.at(index)}`);    
+
+// charCodeAt() method
+const sentence="My name is Abdullah ansari"
+index=3
+console.log(`this is ${sentence.charCodeAt(index)} and ${sentence.at(index)}`);
