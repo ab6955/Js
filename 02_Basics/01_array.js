ac530eaa58ba02a2d6cd554bf3385948376b2c78
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-15 12:29:44 +0000

    Arrays done

--- 02_Basics/01_array.js
+++ 02_Basics/01_array.js
@@ -1,23 +1,23 @@
 // const myArr=[0,1,2,3,4]
 const myArr=new Array(0,1,2,3,4,5)
-// console.log(myArr);
-// console.log(myArr[1]);
+console.log(myArr);
+console.log(myArr[1]);
 
-// const myArr2=["Mohammad","Abubakr","Umr","Usman","Ali"]
-const myArr2=new Array("Mohammad","Abubakr","Umr","Usman","Ali")
-// console.log(myArr2);
-// console.log(myArr2[3]);
+// const myArr2=["Mohammad","Abubakr","Umar","Usman","Ali"]
+const myArr2=new Array("Mohammad","Abubakr","Umar","Usman","Ali")
+console.log(myArr2);
+console.log(myArr2[3]);
 
-// console.log(myArr.length);
-// console.log(myArr2.length);
+console.log(myArr.length);
+console.log(myArr2.length);
 
 // Array Methods
 
 // 1)push and pop
 myArr.push(33)
-// console.log(myArr);
+console.log(myArr);
 myArr.pop()
-// console.log(myArr);
+console.log(myArr);
 
 // 3) unshift and shift
 myArr2.unshift("Abdullah")
@@ -25,3 +25,24 @@ console.log(myArr2);
 
 myArr2.shift()
 console.log(myArr2);
+
+// 4) includes and indexof
+console.log(myArr.includes(6));
+console.log(myArr2.indexOf("Umar"));
+
+// 5) join 
+const  myArr3=myArr.join()
+console.log(myArr);
+// console.log(myArr3)/;
+console.log(typeof myArr3);
+
+// 6) slice and splice
+const Arr1=myArr.slice(1,3)
+console.log(myArr);
+console.log(Arr1);
+
+const Arr2=myArr.splice(1,3)
+console.log(myArr);
+console.log(Arr2);
+
+// Slice does not manipulate the original array but splice does
