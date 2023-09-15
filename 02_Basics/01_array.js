69c06ea1518661a5ea31bf350eaae60ed046e0da
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-09-15 10:58:12 +0000

    Arrays

---
+++ 02_Basics/01_array.js
@@ -0,0 +1,27 @@
+// const myArr=[0,1,2,3,4]
+const myArr=new Array(0,1,2,3,4,5)
+// console.log(myArr);
+// console.log(myArr[1]);
+
+// const myArr2=["Mohammad","Abubakr","Umr","Usman","Ali"]
+const myArr2=new Array("Mohammad","Abubakr","Umr","Usman","Ali")
+// console.log(myArr2);
+// console.log(myArr2[3]);
+
+// console.log(myArr.length);
+// console.log(myArr2.length);
+
+// Array Methods
+
+// 1)push and pop
+myArr.push(33)
+// console.log(myArr);
+myArr.pop()
+// console.log(myArr);
+
+// 3) unshift and shift
+myArr2.unshift("Abdullah")
+console.log(myArr2);
+
+myArr2.shift()
+console.log(myArr2);
