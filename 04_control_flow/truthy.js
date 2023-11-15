bbd3a76a7bec933d5df1120e99a20def7c3836b2
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-11-15 10:54:31 +0000

    Truthy and falsy values

---
+++ 04_control_flow/truthy.js
@@ -0,0 +1,14 @@
+const email=9
+if(email){
+    console.log("Got email");
+}else{
+    console.log("Do not get email");
+}
+
+
+// falsy value
+// false , 0 , -0 , "" , BigInt 0n ,   null , undefined , Nan , undefined , Nan
+
+// truthy value 
+// true , "0" , "false" , " " , {} , []  , function(){}
+
