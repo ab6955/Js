c7bb80a63c87abfa48fbe31baa7c7d40a14da3fd
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-11-15 12:10:34 +0000

    Truthy and falsy

--- 04_control_flow/truthy.js
+++ 04_control_flow/truthy.js
@@ -1,7 +1,7 @@
-const email=9
-if(email){
+const email = 9
+if (email) {
     console.log("Got email");
-}else{
+} else {
     console.log("Do not get email");
 }
 
@@ -12,3 +12,33 @@ if(email){
 // truthy value 
 // true , "0" , "false" , " " , {} , []  , function(){}
 
+
+const userEmail = []
+if (userEmail.length === 0) {  // checking conditons for array
+    console.log("Array is Empty");
+}
+else {
+    console.log("Array is not empty");
+}
+
+const emptyObj={}
+if(Object.keys(emptyObj).length===0){  // checking for objects
+    console.log("Object is empty");
+}
+else{
+    console.log("Object is not empty");
+}
+
+
+
+//  Nullish coalescing operator (??):    // very easy..
+let val1
+
+// val1=12 ?? 13
+// val1=null ?? 14
+val1=undefined ?? 12
+console.log(val1);
+
+// Ternary operator
+const coffeePrice=10
+coffeePrice>80 ? console.log("Greater than 80") : console.log("Less than 80");
