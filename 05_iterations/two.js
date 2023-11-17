c5d8f3dd1d86fc27ab35f2a0c6b0430f65057f9b
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-11-17 14:05:35 +0000

    do and do while loop

---
+++ 05_iterations/two.js
@@ -0,0 +1,23 @@
+// while
+let index=0;
+while (index<=10) {
+    // console.log(`Value of index is ${index}`);
+    // index=index+3;
+    index=index+2;
+}
+
+const myArr=["a","b","c"]
+let arr=0
+while(arr<myArr.length){
+    // console.log(`Value is ${myArr[arr]}`);
+    arr++;
+}
+
+
+// do while
+
+let score=11;
+do {
+    console.log(`Value is ${score}`);
+    score++;
+} while (score<=10);
