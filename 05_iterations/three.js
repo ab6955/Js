b1c41e8fe290e45c94ac8392f62dff799bd561fa
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2023-11-17 14:39:53 +0000

    for of loop and map

---
+++ 05_iterations/three.js
@@ -0,0 +1,25 @@
+// for of loop
+const Arr=["Batman",1,2,3,4,5,"Superman"]
+for (const value of Arr) {
+    // console.log(value)
+    // console.log(`Value is ${value}`);
+}
+
+const name="My name is Abdullah"
+for (const num of name) {
+    // console.log(`Char is ${num}`);
+}
+
+
+const map=new Map(  )
+map.set("IN","India")
+map.set("US","United stated")
+map.set("PL","Free palestine")
+map.set("IN","India")
+
+// console.log(map);
+
+for (const [key,value] of map) {
+    // console.log(key,"=>",value);
+    console.log(`${key} => ${value}`);
+}
