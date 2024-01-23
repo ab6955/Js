c50a646e1a5538141e26111ff9d85c84c928000e
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2024-01-23 14:35:17 +0000

    Variables done

---
+++ Practice/Basics/1_variables.js
@@ -0,0 +1,8 @@
+const studentId="vu2f2122045"
+let studentemail="3434@pvppacoe@gmail.com"
+let studentCity="Mumbai"
+let studentState;
+
+console.log(studentId,studentemail,studentCity,studentState);
+console.log([studentId,studentemail,studentCity,studentState]);
+console.table([studentId,studentemail,studentCity,studentState]);  // Displat data in tabular form
