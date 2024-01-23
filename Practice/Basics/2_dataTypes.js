67c361cc07f5bc619271831cea11cea8c56ba444
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2024-01-23 15:14:53 +0000

    Data types done

---
+++ Practice/Basics/2_dataTypes.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const studentId=45
+let studentName="Abdullah"
+let studentCity="Mumbai"
+
+console.log(typeof studentId);
+console.log(typeof null);  // type of null is object.
+console.log(typeof undefined); //type of undefined is undefined.
