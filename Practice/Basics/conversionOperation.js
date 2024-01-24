155623189615036967e1a8290b86f683501a6321
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2024-01-24 14:57:22 +0000

    Convsersion Done

--- Practice/Basics/conversionOperation.js
+++ Practice/Basics/conversionOperation.js
@@ -1,6 +1,21 @@
-let score ="33"
-console.log(typeof score);
+let score =""
+// console.log(typeof score);
 
 let scoreInNumber=Number(score)
-console.log(scoreInNumber);
-console.log(typeof scoreInNumber);
+// console.log(scoreInNumber);
+// console.log(typeof scoreInNumber);
+
+// "33"=> 33
+// "33aa"=>Nan
+// true=>1 ; false=>0.
+
+// let isLoggedIn=2
+let isLoggedIn=undefined
+console.log(typeof isLoggedIn);
+let booleanIsLoggedIn=Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn);
+console.log(typeof booleanIsLoggedIn);
+
+// 1=> true ; 0=>false
+// "Abdullah"=> true
+// "",null,undefined=> false
