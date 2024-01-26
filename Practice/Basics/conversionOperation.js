165c1fa37deddebb2285c16a071203a3a0a51514
ansariabd62 <143590602+ansariabd62@users.noreply.github.com>
2024-01-26 14:44:00 +0000

    Conversion Done

--- Practice/Basics/conversionOperation.js
+++ Practice/Basics/conversionOperation.js
@@ -1,4 +1,5 @@
 let score =""
+// console.log(score);
 // console.log(typeof score);
 
 let scoreInNumber=Number(score)
@@ -11,11 +12,38 @@ let scoreInNumber=Number(score)
 
 // let isLoggedIn=2
 let isLoggedIn=undefined
-console.log(typeof isLoggedIn);
+// console.log(typeof isLoggedIn);
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
-console.log(typeof booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
+// console.log(typeof booleanIsLoggedIn);
 
 // 1=> true ; 0=>false
 // "Abdullah"=> true
 // "",null,undefined=> false
+
+
+
+// ***************** Opeartions//////////
+let firstName="Abdullah"
+let lastName=" Ansari"
+let fullName=firstName+lastName
+// console.log(fullName);
+// console.log(firstName+lastName);
+
+// console.log(1+2);
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+2+"2");
+// console.log("1"+2+2);
+
+let x = 3;
+const y = x++;
+
+console.log(`x:${x}, y:${y}`);
+
+
+
+let a = 3;
+const b = ++a;
+
+console.log(`a:${a}, b:${b}`);
