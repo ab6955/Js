52401a4f3bd4dc1598f38183864ba3099ae0a276
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-01-31 13:09:41 +0000

    string

---
+++ Practice/Basics/5_String.js
@@ -0,0 +1,14 @@
+let name="Abdullah"
+let age=20
+
+// String interpolation..
+
+// console.log(`Hello my name is ${name} and I am ${age} years old.`);
+
+const gameName=new String("Hello")  //Another method to declare string.Here string treated as Object. 
+// console.log(typeof gameName);
+
+console.log(gameName.toUpperCase());
+console.log(gameName.toLowerCase());
+console.log(name.indexOf('u'));
+console.log(gameName.charAt(3));
