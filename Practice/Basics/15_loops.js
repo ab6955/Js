bf12efca39d07ed58afe28c5c2e03d6cde5d3772
Abdullah Ansari <143590602+ab6955@users.noreply.github.com>
2024-02-12 19:46:34 +0000

    Loops

--- Practice/Basics/15_loops.js
+++ Practice/Basics/15_loops.js
@@ -152,6 +152,53 @@ const coding = [
     }
 ]
 
-coding.forEach( (item)=>{
-    console.log(item.programmingPrice);
+coding.forEach((item) => {
+    // console.log(item.programmingPrice);
 })
+
+
+// ********* Filter **********
+
+
+const coding2 = ["js", "py", 'java']
+const result = coding2.forEach((item) => {
+    // console.log(item);
+    return item;
+})
+// console.log(result);   // for each loop does not returning anything.
+
+
+const num = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// const newNum = num.filter((num) => num > 4)
+const newNum = num.filter((num) => {    //if using {} then must use return keyword.
+    return num > 4
+})
+// console.log(newNum);
+
+const newNum2 = []
+
+num.forEach((n1) => {
+    if (n1 >= 4) {
+        newNum2.push(n1)
+    }
+})
+// console.log(newNum2);   // This and the above one (filter) are totally same.
+
+const books = [
+    { title: 'Book One', genre: 'Fiction', publish: 1981, edition: 2004 },
+    { title: 'Book Two', genre: 'Non-Fiction', publish: 1992, edition: 2008 },
+    { title: 'Book Three', genre: 'History', publish: 1999, edition: 2007 },
+    { title: 'Book Four', genre: 'Non-Fiction', publish: 1989, edition: 2010 },
+    { title: 'Book Five', genre: 'Science', publish: 2009, edition: 2014 },
+    { title: 'Book Six', genre: 'Fiction', publish: 1987, edition: 2010 },
+    { title: 'Book Seven', genre: 'History', publish: 1986, edition: 1996 },
+    { title: 'Book Eight', genre: 'Science', publish: 2011, edition: 2016 },
+    { title: 'Book Nine', genre: 'Non-Fiction', publish: 1981, edition: 1989 },
+];
+
+
+let bookdata = books.filter((b) => b.edition > 2000)
+console.log(bookdata);
+
+// **************** 
